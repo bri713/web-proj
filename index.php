4a6813fdb4ebf2a15de1d859a4de8154fef64e6a
bri713 <brian713ng@gmail.com>
2021-02-19 19:39:57 +0800

    login

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-echo "try"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="frm">
+        <form action= "process.php" method="POST">
+            <p>
+                <label>Username:</label>
+                <Input type="text" id="user" name="user"  />
+            </p>
+            <p>
+                <label>Password:</label>
+                <Input type="password" id="pass" name="pass"  />
+            </p>
+            <p>
+                <Input type="submit" id="btn" name="Login"  />
+            </p>
+        </form>
+    </div>
+</body>
+</html>
