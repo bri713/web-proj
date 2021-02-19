81a6cdf815a2ec470c6b014045d19e8488f0c1e5
bri713 <brian713ng@gmail.com>
2021-02-19 22:23:28 +0800

    process

--- process.php
+++ process.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     $db = parse_url(getenv("DATABASE_URL"));
     
@@ -20,3 +29,6 @@
     }else{
         echo "UNSUCCESS";
     }
+</body>
+</html>
+
