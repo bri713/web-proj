4600cd97f4c17b07e35dab7f279fa69a391ebb59
bri713 <brian713ng@gmail.com>
2021-02-19 22:41:53 +0800

    process

--- process.php
+++ process.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
     $db = parse_url(getenv("DATABASE_URL"));
 
@@ -21,7 +13,7 @@
     echo "Hello DB user";
     echo db["user"];
     
-    $sql = "Insert INTO Users (username, passwords) VALUES (HI, HI)"
+    $sql = "INSERT INTO Users (username, passwords) VALUES (HI, HI)";
     
     if($pdo->query($sql)==TRUE)
     {
@@ -30,6 +22,5 @@
         echo "UNSUCCESS";
     }
 ?>
-</body>
-</html>
+
 
